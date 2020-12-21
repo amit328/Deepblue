4d9519aa3173d85bb3db9a14a85109eb3883d6b4
amit328 <51810663+amit328@users.noreply.github.com>
2020-12-21 10:03:50 +0530

    project to glitch

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 // console.log(process.env);
 const app = express();
 app.use(cors());
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {console.log(`Starting server at ${port}`)});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
